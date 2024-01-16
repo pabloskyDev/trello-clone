6fe428f52efa25033256bf349ad905011f2820b0
Juan Pablo Betancur M <jpbetancur1999@gmail.com>
2024-01-15 23:34:16 -0500

    [UPD] Finish login section

--- src/app/pages/auth/login/login.component.ts
+++ src/app/pages/auth/login/login.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { IconComponent } from '@components/icon/icon.component';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [IconComponent],
   templateUrl: './login.component.html',
   styles: [
   ]

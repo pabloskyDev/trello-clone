eeccce20d0b6aa7b0a0a056d47d7dfc0547c42ec
Juan Pablo Betancur M <jpbetancur1999@gmail.com>
2024-01-16 23:19:27 -0500

    [UPD] Implement button component

--- src/app/pages/auth/login/login.component.ts
+++ src/app/pages/auth/login/login.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
+import { ButtonComponent } from '@components/button/button.component';
 import { IconComponent } from '@components/icon/icon.component';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [IconComponent],
+  imports: [IconComponent, ButtonComponent],
   templateUrl: './login.component.html',
   styles: [
   ]
